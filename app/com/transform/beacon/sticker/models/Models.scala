98027b64c656f19e898d40767a238fc5ac84b2c8
IrfanAnsari <irfanaansari@gmail.com>
2015-04-29 10:20:36 +0100

    Removed unused code

--- app/com/transform/beacon/sticker/models/Models.scala
+++ app/com/transform/beacon/sticker/models/Models.scala
@@ -15,25 +15,10 @@ object Sticker {
 
 case class NPSLocatable(_id: Option[String], identifier: String, rssi: Int, _type: String)
 
-object NPSLocatable {
-
-  implicit val npsLocatableWriter = Json.writes[NPSLocatable]
-  implicit val npsLocatableReader = Json.reads[NPSLocatable]
-}
-
 case class NPSEvent(_id: Option[String], identifier: String, npsLocatables: List[NPSLocatable]) extends Model
 
 object NPSEvent {
-
-  implicit val npsEventWriter = Json.writes[NPSEvent]
-  implicit val npsEventReader = Json.reads[NPSEvent]
-
-  implicit val npsLocatableWriter = Json.writes[NPSLocatable]
-  implicit val npsLocatableReader = Json.reads[NPSLocatable]
-
-//
-//
-//  implicit val npsLocatableFormat = Json.format[NPSLocatable]
+  implicit val npsLocatableFormat = Json.format[NPSLocatable]
   implicit val npsEventFormat = Json.format[NPSEvent]
 }
 
